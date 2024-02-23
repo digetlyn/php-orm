c215d914bb4f79f53baf717e452c9eceb9f30f10
dyadya <digetlyn@naver.com>
2024-02-24 01:33:46 +0900

    laravel Migration마이그레이션 작업

---
+++ database/migrations/2024_02_23_161759_create_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject',255); // subject varchar(255) 이거를 만들다? subject필드가 생성될때 만들어진다..
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
